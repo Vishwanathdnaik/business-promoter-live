49083359fc1467bb6f86c539be92381932066bf3
Vishwanath D N <writetous@businesspromoter.co.in>
2025-06-04 10:57:44 +0530

    📬 Added ContactForm below CTA with all fields

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,17 @@
-// src/pages/index.tsx
-
-import Head from 'next/head'
 import Hero from '@/components/Hero'
 import Features from '@/components/Features'
 import CTA from '@/components/CTA'
+import ContactForm from '@/components/ContactForm'
 import Footer from '@/components/Footer'
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Business Promoter | Grow Your Business Today</title>
-        <meta name="description" content="Boost your online presence and generate more leads with Business Promoter solutions." />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Hero />
-        <Features />
-        <CTA />
-        <Footer />
-      </main>
+      <Hero />
+      <Features />
+      <CTA />
+      <ContactForm />
+      <Footer />
     </>
   )
 }
