f821058ea114c4c9686d24a9c33c60b716c227c7
Vishwanath D N <writetous@businesspromoter.co.in>
2025-06-05 23:19:11 +0530

    ✅ Navbar Live: Navigation UI integrated & built

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Navbar from '@/components/Navbar';
+import Hero from '@/components/Hero';
+import ServicesList from '@/components/ServicesList';
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <ServicesList />
+    </>
+  );
+}
