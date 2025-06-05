a0069434f3f5b966de413babdeb1e7d9c0790c83
Vishwanath D N <writetous@businesspromoter.co.in>
2025-06-06 01:44:38 +0530

    🔥 Fix: Default export added to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
-import Navbar from '@/components/Navbar';
-import Hero from '@/components/Hero';
-import ServicesList from '@/components/ServicesList';
+// pages/index.js
+import React from 'react'
 
 export default function Home() {
   return (
-    <>
-      <Navbar />
-      <Hero />
-      <ServicesList />
-    </>
-  );
+    <div className="text-center mt-20">
+      <h1 className="text-4xl font-bold">Business Promoter Hub 🚀</h1>
+      <p className="mt-2 text-lg text-gray-700">
+        Your one-stop platform for digital services
+      </p>
+    </div>
+  )
 }
