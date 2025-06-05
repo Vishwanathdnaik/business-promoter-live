af539b59829f34a91b5e549be703d8ae72c6df87
Vishwanath D N <writetous@businesspromoter.co.in>
2025-06-05 16:06:22 +0530

    💄 Polish: Tailwind layout upgrade

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import Hero from '@/components/Hero'
 import ServicesList from '@/components/ServicesList'
+import Footer from '@/components/Footer'
 
 export default function Home() {
   return (
-    <>
+    <main className="min-h-screen bg-gray-50 text-gray-800">
       <Hero />
       <ServicesList />
-    </>
+      <Footer />
+    </main>
   )
 }
